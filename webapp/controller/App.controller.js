f81138917554d20f431c5c10a57dec09d9c6d754
tganz <tganz5890@googlemail.com>
2021-08-14 07:14:29 +0200

    add nested view

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,4 @@
-sap.ui.define(
-  ['sap/ui/core/mvc/Controller', 'sap/m/MessageToast'],
-  function (Controller, MessageToast) {
-    'use strict';
-    return Controller.extend('sap.ui.demo.walkthrough.controller.App', {
-      onShowHello: function () {
-        MessageToast.show('Hello World');
-      }
-    });
-  }
-);
+sap.ui.define(['sap/ui/core/mvc/Controller'], function (Controller) {
+  'use strict';
+  return Controller.extend('sap.ui.demo.walkthrough.controller.App', {});
+});
